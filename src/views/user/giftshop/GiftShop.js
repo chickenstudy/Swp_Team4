c9e5b47051d2a4035392bc8b5abdff989b172da5
ThaiToni <vietthaist01@gmail.com>
2023-06-20 14:32:57 +0700

    xoa

--- src/views/user/giftshop/GiftShop.js
+++
@@ -1,6 +0,0 @@
-
-
-export default function GiftShop() {
-  return
-   <p> GIFT</p>
-}
--- src/views/user/promotion/Promotion.js
+++
@@ -1,7 +0,0 @@
-
-export default function Promotion() {
-    return (
-        <p>  Promotions</p>
-    )
-  }
-  
