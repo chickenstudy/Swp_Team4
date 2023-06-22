b5d691975a56c539c7b01b05afe52cbe16544882
NPNem <ngphnam47@gmail.com>
2023-06-23 05:40:34 +0700

    listmovie

--- src/main/java/com/namnguyenmoihoc/realworldapp/controller/MovieController.java
+++ src/main/java/com/namnguyenmoihoc/realworldapp/controller/MovieController.java
@@ -4,14 +4,19 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.namnguyenmoihoc.realworldapp.exception.custom.CustomNotFoundException;
 import com.namnguyenmoihoc.realworldapp.model.movie.MovieDTOCreate;
 import com.namnguyenmoihoc.realworldapp.model.movie.MovieDTOResponse;
+import com.namnguyenmoihoc.realworldapp.model.profileAccount.ProfileDTOResponsive;
 import com.namnguyenmoihoc.realworldapp.model.roles.UserRolesDTOResponse;
+import com.namnguyenmoihoc.realworldapp.model.user.dto.UserDTOUpdateAccount;
 import com.namnguyenmoihoc.realworldapp.service.MovieService;
 
 import lombok.RequiredArgsConstructor;
@@ -32,6 +37,18 @@ public class MovieController {
     public List<MovieDTOResponse> getListMovie(){
         return movieService.getListMovie();
     }
+
+    /*
+     * @PutMapping("/updateMovie/{movieId}")
+    public Map<String, ProfileDTOResponsive> getUpdateMovie(@PathVariable int movieId,
+            @RequestBody UserDTOUpdateAccount userDTOUpdateAccount)
+            throws CustomNotFoundException {
+        System.out.println(userDTOUpdateAccount);
+        userDTOUpdateAccount.setId(userid);
+        return userService.getUpdateAccount(userDTOUpdateAccount);
+    }
+     */
+    
 
 
     
