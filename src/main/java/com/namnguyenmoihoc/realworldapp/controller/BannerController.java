5e2a7f00c97c35cae58e12bf138c298008feea2a
unknown <quangnnhe171324@fpt.edu.vn>
2023-06-30 00:45:58 +0700

    update cross origin banner

--- src/main/java/com/namnguyenmoihoc/realworldapp/controller/BannerController.java
+++ src/main/java/com/namnguyenmoihoc/realworldapp/controller/BannerController.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/banner")
+@CrossOrigin
 @RequiredArgsConstructor
 public class BannerController {
     private final BannerService bannerService;
