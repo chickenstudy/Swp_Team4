b78ec46ab56fd9f5538e70a2e38e4ec4a6e2bf26
NPNem <ngphnam47@gmail.com>
2023-07-01 02:29:23 +0700

    update isAcive in banner table

--- Backend(Spring-social)/src/main/java/com/namnguyenmoihoc/realworldapp/entity/Cinema.java
+++ Backend(Spring-social)/src/main/java/com/namnguyenmoihoc/realworldapp/entity/Cinema.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
