d9c4cc6d4047f28419c4a5a5d5015804665963da
Chicken <tuannnhe163137@fpt.edu.vn>
2023-07-02 01:38:23 +0700

    loading seat booking .....

---
+++ Frontend(React-social)/src/views/user/movies/Seat.js
@@ -0,0 +1,248 @@
+import React from "react";
+import "./seat.css";
+
+class Seat extends React.Component {
+  render() {
+    return (
+      <div style={{ textAlign: "center", color: "pink" }}>
+        <div class="container">
+          <div class="row">
+            <div class="row-mark">A</div>
+            <div class="seat-blank default row-A col-1"></div>
+            <div class="seat-blank default row-A col-2"></div>
+            <div class="seat default row-A col-3"></div>
+            <div class="seat default row-A col-4"></div>
+            <div class="seat default row-A col-5"></div>
+            <div class="seat default row-A col-6"></div>
+            <div class="seat default row-A col-7"></div>
+            <div class="seat default row-A col-8"></div>
+            <div class="seat default row-A col-9"></div>
+            <div class="seat default row-A col-10"></div>
+            <div class="seat default row-A col-11"></div>
+            <div class="seat default row-A col-12"></div>
+            <div class="seat default row-A col-13"></div>
+            <div class="seat default row-A col-14"></div>
+            <div class="seat default row-A col-15"></div>
+            <div class="seat default row-A col-16"></div>
+            <div class="seat-blank default row-A col-17"></div>
+            <div class="seat-blank default row-A col-18"></div>
+          </div>
+          <div class="row">
+            <div class="row-mark">B</div>
+            <div class="seat-blank"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat occupied"></div>
+            <div class="seat occupied"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat-blank"></div>
+          </div>
+          <div class="row">
+            <div class="row-mark">C</div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat occupied"></div>
+            <div class="seat occupied"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+          </div>
+          <div class="row">
+            <div class="row-mark">D</div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat occupied"></div>
+            <div class="seat occupied"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+          </div>
+          <div class="row">
+            <div class="row-mark">E</div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat occupied"></div>
+            <div class="seat occupied"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+          </div>
+          <div class="row">
+            <div class="row-mark">F</div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat occupied"></div>
+            <div class="seat occupied"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+          </div>
+          <div class="row">
+            <div class="row-mark">G</div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat occupied"></div>
+            <div class="seat occupied"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+          </div>
+          <div class="row">
+            <div class="row-mark">H</div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat occupied"></div>
+            <div class="seat occupied"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+          </div>
+          <div class="row">
+            <div class="row-mark">I</div>
+            <div class="seat-blank"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat occupied"></div>
+            <div class="seat occupied"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat-blank"></div>
+          </div>
+          <div class="row">
+            <div class="row-mark">J</div>
+            <div class="seat-blank"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat occupied"></div>
+            <div class="seat occupied"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat vip"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat"></div>
+            <div class="seat-blank"></div>
+          </div>
+          <div class="row-container">
+            <div class="row">
+              <div class="row-mark"></div>
+              <div class="seat-col">1</div>
+              <div class="seat-col">2</div>
+              <div class="seat-col">3</div>
+              <div class="seat-col">4</div>
+              <div class="seat-col">5</div>
+              <div class="seat-col">6</div>
+              <div class="seat-col">7</div>
+              <div class="seat-col">8</div>
+              <div class="seat-col">9</div>
+              <div class="seat-col">10</div>
+              <div class="seat-col">11</div>
+              <div class="seat-col">12</div>
+              <div class="seat-col">13</div>
+              <div class="seat-col">14</div>
+              <div class="seat-col">15</div>
+              <div class="seat-col">16</div>
+              <div class="seat-col">17</div>
+              <div class="seat-col">18</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Seat;
