bb2e38de63cbbacd1569557e343e0652bdb4637c
VsHoang <hoangbboy2210@gmail.com>
2023-07-01 20:45:39 +0700

    RUD showtime

--- Backend(Spring-social)/src/main/java/com/namnguyenmoihoc/realworldapp/service/ShowtimeService.java
+++ Backend(Spring-social)/src/main/java/com/namnguyenmoihoc/realworldapp/service/ShowtimeService.java
@@ -1,15 +1,23 @@
 package com.namnguyenmoihoc.realworldapp.service;
 
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 import java.util.Map;
 
+import com.namnguyenmoihoc.realworldapp.exception.custom.CustomNotFoundException;
 import com.namnguyenmoihoc.realworldapp.model.Showtime.ShowtimeDTOCreate;
-
+import com.namnguyenmoihoc.realworldapp.model.Showtime.ShowtimeDTOResponse;
 import com.namnguyenmoihoc.realworldapp.model.Showtime.ShowtimeDTOResponseNoID;
 
 public interface ShowtimeService {
 
     Map<String, ShowtimeDTOResponseNoID> createShowtime(Map<String, ShowtimeDTOCreate> showtimeDTOCreate) throws UnsupportedEncodingException;
+
+    List<ShowtimeDTOResponse> getListShowtime();
+
+    Map<String, ShowtimeDTOResponseNoID> getUpdateShowtime(ShowtimeDTOCreate showtimeDTOCreate)   throws CustomNotFoundException;
+
+    void getDeleteShowtime(int showtimeId)  throws CustomNotFoundException;
 
    
     
