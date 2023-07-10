99e660edf3c78908aadb8635f86f9eaf6505ad71
VsHoang <hoangbboy2210@gmail.com>
2023-07-10 13:29:37 +0700

    test cinema and seat

--- Backend(Spring-social)/src/test/java/com/namnguyenmoihoc/realworldapp/service/impl/SeatServiceImplTest.java
+++ Backend(Spring-social)/src/test/java/com/namnguyenmoihoc/realworldapp/service/impl/SeatServiceImplTest.java
@@ -1,10 +1,76 @@
 package com.namnguyenmoihoc.realworldapp.service.impl;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import com.namnguyenmoihoc.realworldapp.entity.Seat;
+
+import com.namnguyenmoihoc.realworldapp.model.seat.SeatDTOResponse;
+
+import com.namnguyenmoihoc.realworldapp.repository.SeatRepositorty;
+
+@SpringBootTest
 public class SeatServiceImplTest {
+    
+    @InjectMocks
+    private SeatServiceImpl seatService;
+
+    @Mock
+    private SeatRepositorty seatRepository;
+
     @Test
     void testGetListSeats() {
+          Seat seat1 = new Seat();
+        seat1.setSeatid(1);;
+        seat1.setPrice(10000);
+        seat1.setCol("A");
+        seat1.setRow("1");
+        
+  Seat seat2 = new Seat();
+        seat2.setSeatid(2);;
+        seat2.setPrice(10000);
+        seat2.setCol("B");
+        seat2.setRow("2");
+
+        // Create a list of cinema objects
+        List<Seat> listSeat = new ArrayList<>();
+        listSeat.add(seat1);
+        listSeat.add(seat2);
+
+        // Set up the mock behavior for the cinema repository
+        when(seatRepository.findAll()).thenReturn(listSeat);
+
+        // Call the getListCinemas() method to test
+        List<SeatDTOResponse> result = seatService.getListSeats();
+
+        // Check the result
+        assertNotNull(result);
+        assertEquals(2, result.size()); // Check that the result list has 2 elements
+
+        SeatDTOResponse seatDTO1 = result.get(0);
+        assertNotNull(seatDTO1);
+        assertEquals(seat1.getSeatid(), seatDTO1.getSeatid());
+        assertEquals(seat1.getPrice(), seatDTO1.getPrice());
+        assertEquals(seat1.getCol(), seatDTO1.getCol());
+         assertEquals(seat1.getRow(), seatDTO1.getRow());
+
+
+          SeatDTOResponse seatDTO2 = result.get(1);
+        assertNotNull(seatDTO2);
+        assertEquals(seat2.getSeatid(), seatDTO2.getSeatid());
+        assertEquals(seat2.getPrice(), seatDTO2.getPrice());
+        assertEquals(seat2.getCol(), seatDTO2.getCol());
+         assertEquals(seat2.getRow(), seatDTO2.getRow());
 
     }
 }
--- Backend(Spring-social)/src/test/java/com/namnguyenmoihoc/realworldapp/service/impl/ShowtimeServiceImplTest.java
+++ Backend(Spring-social)/src/test/java/com/namnguyenmoihoc/realworldapp/service/impl/ShowtimeServiceImplTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -22,110 +23,106 @@ import com.namnguyenmoihoc.realworldapp.entity.Cinema;
 import com.namnguyenmoihoc.realworldapp.entity.Showtime;
 import com.namnguyenmoihoc.realworldapp.exception.custom.CustomNotFoundException;
 import com.namnguyenmoihoc.realworldapp.model.Showtime.ShowtimeDTOCreate;
+import com.namnguyenmoihoc.realworldapp.model.Showtime.ShowtimeDTOResponse;
 import com.namnguyenmoihoc.realworldapp.model.Showtime.ShowtimeDTOResponseNoID;
-import com.namnguyenmoihoc.realworldapp.model.cinema.CinemaDTO;
-import com.namnguyenmoihoc.realworldapp.model.cinema.CinemaDTOResponse;
-import com.namnguyenmoihoc.realworldapp.model.cinema.CinemaDTOResponseNoId;
-import com.namnguyenmoihoc.realworldapp.model.user.mapper.CinemaMapper;
+
 import com.namnguyenmoihoc.realworldapp.model.user.mapper.ShowtimeMapper;
-import com.namnguyenmoihoc.realworldapp.repository.CinemaRepository;
+
 import com.namnguyenmoihoc.realworldapp.repository.ShowtimeRepository;
 
 @SpringBootTest
 public class ShowtimeServiceImplTest {
-    @InjectMocks
-    private ShowtimeServiceImpl showtimeServiceImpl;
+  @InjectMocks
+  private ShowtimeServiceImpl showtimeServiceImpl;
 
-    @Mock
-    private ShowtimeRepository showtimeRepository;
+  @Mock
+  private ShowtimeRepository showtimeRepository;
 
-    @Test
-    void testCreateShowtime() {
-        ShowtimeDTOCreate showtimeDTOCreate = new ShowtimeDTOCreate();
-        showtimeDTOCreate.setStarttime("7h");
-        showtimeDTOCreate.setEndtime("9h");
+  @Test
+  void testCreateShowtime() {
+    ShowtimeDTOCreate showtimeDTOCreate = new ShowtimeDTOCreate();
+    showtimeDTOCreate.setStarttime("7h");
+    showtimeDTOCreate.setEndtime("9h");
 
-        // Create the cinema DTO map and add the cinema DTO to it
-        Map<String, ShowtimeDTOCreate> showMap = new HashMap<>();
-        showMap.put("showtime", showtimeDTOCreate);
+    // Create the cinema DTO map and add the cinema DTO to it
+    Map<String, ShowtimeDTOCreate> showMap = new HashMap<>();
+    showMap.put("showtime", showtimeDTOCreate);
 
-        // Convert the cinema DTO to a cinema object
-        Showtime showtime = ShowtimeMapper.toShowtime(showtimeDTOCreate);
+    // Convert the cinema DTO to a cinema object
+    Showtime showtime = ShowtimeMapper.toShowtime(showtimeDTOCreate);
 
-        // Set up the mock behavior for the cinema repository
-        when(showtimeRepository.save(any(Showtime.class))).thenReturn(showtime);
+    // Set up the mock behavior for the cinema repository
+    when(showtimeRepository.save(any(Showtime.class))).thenReturn(showtime);
 
-        // Call the createCinema() method to test
-        Map<String, ShowtimeDTOResponseNoID> result = showtimeServiceImpl.createShowtime(showMap);
+    // Call the createCinema() method to test
+    Map<String, ShowtimeDTOResponseNoID> result = showtimeServiceImpl.createShowtime(showMap);
 
-        // Check the result
-        assertEquals(1, result.size()); // Check that the result map has 1 element
-        assertTrue(result.containsKey("showtime"));
+    // Check the result
+    assertEquals(1, result.size()); // Check that the result map has 1 element
+    assertTrue(result.containsKey("showtime"));
 
-    }
+  }
 
-    @Test
-    void testGetDeleteShowtime() throws CustomNotFoundException {
+  @Test
+  void testGetDeleteShowtime() throws CustomNotFoundException {
 
-        Showtime showtime = new Showtime();
-        showtime.setShowtimeid(1);
-        ;
+    Showtime showtime = new Showtime();
+    showtime.setShowtimeid(1);
+    ;
 
-        // Mock the movieRepository.findById() method to return the movie
-        when(showtimeRepository.findById(1)).thenReturn(Optional.of(showtime));
+    // Mock the movieRepository.findById() method to return the movie
+    when(showtimeRepository.findById(1)).thenReturn(Optional.of(showtime));
 
-        // Call the method under test
-      showtimeServiceImpl.getDeleteShowtime(1);;
-        ;
+    // Call the method under test
+    showtimeServiceImpl.getDeleteShowtime(1);
+    
 
-        // Verify the behavior
-        verify(showtimeRepository).findById(1); // Verify that movieRepository.findById() was called
-        verify(showtimeRepository).deleteById(1);
-    }
+    // Verify the behavior
+    verify(showtimeRepository).findById(1); // Verify that movieRepository.findById() was called
+    verify(showtimeRepository).deleteById(1);
+  }
 
-    @Test
-    void testGetListShowtime() {
-      //  Showtime showtime1 = new Showtime();
-      //  showtime1.setShowtimeid(1);
-      //   showtime1.setStarttime("7h");
-      // showtime1.setEndtime("9h");
+  @Test
+  void testGetListShowtime() {
+    Showtime showtime1 = new Showtime();
+    showtime1.setShowtimeid(1);
+    showtime1.setStarttime("7h");
+    showtime1.setEndtime("9h");
 
-      // Showtime showtime2 = new Showtime();
-      //  showtime2.setShowtimeid(2);
-      //   showtime2.setStarttime("13h");
-      // showtime2.setEndtime("15h");
+    Showtime showtime2 = new Showtime();
+    showtime2.setShowtimeid(2);
+    showtime2.setStarttime("13h");
+    showtime2.setEndtime("15h");
 
-      //   // Create a list of cinema objects
-      //   List<Showtime> listShowtime = new ArrayList<>();
-      //   listShowtime.add(cinema1);
-      //   listCinemas.add(cinema2);
+    // Create a list of cinema objects
+    List<Showtime> listShowtime = new ArrayList<>();
+    listShowtime.add(showtime1);
+    listShowtime.add(showtime2);
 
-      //   // Set up the mock behavior for the cinema repository
-      //   when(cinemaRepository.findAll()).thenReturn(listCinemas);
+    // Set up the mock behavior for the cinema repository
+    when(showtimeRepository.findAll()).thenReturn(listShowtime);
 
-      //   // Call the getListCinemas() method to test
-      //   List<CinemaDTOResponse> result = cinemaService.getListCinemas();
+    // Call the getListCinemas() method to test
+    List<ShowtimeDTOResponse> result = showtimeServiceImpl.getListShowtime();
 
-      //   // Check the result
-      //   assertNotNull(result);
-      //   assertEquals(2, result.size()); // Check that the result list has 2 elements
+    // Check the result
+    assertNotNull(result);
+    assertEquals(2, result.size()); // Check that the result list has 2 elements
 
-      //   CinemaDTOResponse cinemaDTO1 = result.get(0);
-      //   assertNotNull(cinemaDTO1);
-      //   assertEquals(cinema1.getCinemaid(), cinemaDTO1.getCinemaid());
-      //   assertEquals(cinema1.getName(), cinemaDTO1.getName());
-      //   assertEquals(cinema1.getLocation(), cinemaDTO1.getLocation());
+    ShowtimeDTOResponse showtimeDTO1 = result.get(0);
+    assertNotNull(showtimeDTO1);
+    assertEquals(showtime1.getShowtimeid(), showtimeDTO1.getShowtimeid());
+    assertEquals(showtime1.getStarttime(), showtimeDTO1.getStarttime());
+    assertEquals(showtime1.getEndtime(), showtimeDTO1.getEndtime());
 
-      //   CinemaDTOResponse cinemaDTO2 = result.get(1);
-      //   assertNotNull(cinemaDTO2);
-      //   assertEquals(cinema2.getCinemaid(), cinemaDTO2.getCinemaid());
-      //   assertEquals(cinema2.getName(), cinemaDTO2.getName());
-      //   assertEquals(cinema2.getLocation(), cinemaDTO2.getLocation());
+    ShowtimeDTOResponse showtimeDTO2 = result.get(1);
+    assertNotNull(showtimeDTO2);
+     assertEquals(showtime2.getShowtimeid(), showtimeDTO2.getShowtimeid());
+    assertEquals(showtime2.getStarttime(), showtimeDTO2.getStarttime());
+    assertEquals(showtime2.getEndtime(), showtimeDTO2.getEndtime());
+    
 
-    }
+  }
 
-    @Test
-    void testGetUpdateShowtime() {
-
-    }
+  
 }
